6a17f21c8917876a5fda49d5cf915d7db2408756
kod-er <er.abhiroks@gmail.com>
2023-04-08 22:46:49 +0530

    style: updated logo

--- src/components/brand-logo.js
+++ src/components/brand-logo.js
@@ -5,7 +5,7 @@ import { Flex } from "./ui"
 export default function GatsbyContentfulLogo() {
   return (
     <Flex>
-      <img src={logo} style={{ height: "70px" }} />
+      <img src={logo} style={{ height: "100px" }} />
       {/* <h3>Sadhana Freak</h3> */}
     </Flex>
   )
